8ed3bad9de2253acf66a755ed049966fb81a33ad
Andres Mena <ballerlikemike23@gmail.com>
2019-06-25 21:43:10 -0400

    Flip Coin

--- src/CoinContainer.js
+++ src/CoinContainer.js
@@ -25,21 +25,13 @@ class CoinContainer extends Component {
     // take help function to get a random choice
     const newCoin = choice(this.props.coins)
     this.setState(st => {
-      // create an object with the simple state changes
-      let newState = {
-        ...st, // sets heads and tails as their current values
+      return {
         currCoin: newCoin,
         numFlips: st.numFlips + 1,
+        // easier logic to read than if statement
+        numHeads: st.numHeads + (newCoin.side === 'heads' ? 1 : 0),
+        numTails: st.numTails + (newCoin.side === 'tails' ? 1 : 0)
       }
-      // if statement for the conditional state changes
-      if (newCoin.side === 'heads') {
-        // add one to heads
-        newState.numHeads += 1
-      } else {
-        // add one to tails
-        newState.numTails += 1
-      }
-      return newState
     })
   }
 
@@ -51,7 +43,10 @@ class CoinContainer extends Component {
     return (
       <div>
         <h2>Let's Flip a coin!</h2>
-        <Coin face={this.props.coins[0]} />
+        {
+          this.state.currCoin &&
+          <Coin info={this.state.currCoin} />
+        }
         <button onClick={this.handleClick}>FLIP ME</button>
         <p>Out of {this.state.numFlips} flips, there have been {this.state.numHeads} heads and {this.state.numTails} tails.</p>
       </div>
